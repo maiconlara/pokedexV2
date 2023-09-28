9e7c50d48c40b7b3a25b7e3cfd4f710f0b404da5
maiconlara <maiconluan.lara@yahoo.com.br>
2023-09-28 10:11:38 -0300

    styling scrollbar and creating routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
 "use client";
 
-import Image from "next/image";
-import { NextUIProvider } from "@nextui-org/react";
-import { Button } from "@nextui-org/react";
 
+import { NextUIProvider } from "@nextui-org/react";
+import { useState } from "react";
 
 export default function Home() {
+
   return (
     <>
       <NextUIProvider>
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-          <Button color="warning" className="text-white" isLoading>Carregando</Button>
+        <main className="flex min-h-screen flex-col items-center justify-between">
+
         </main>
       </NextUIProvider>
     </>
