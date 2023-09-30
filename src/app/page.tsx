6f67917a504e1140304d2465c504c1d9bfb65155
Maicon <maiconluan.lara@yahoo.com.br>
2023-09-30 17:20:21 -0300

    nextui/image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,14 @@
 import ButtonC from "./components/Button";
 import background from "./assets/background.webp";
 import backgroundMobile from "./assets/backgroundMobile.webp";
-import Image from "next/image";
+import {Image} from "@nextui-org/react";
 
 export default function Home() {
   return (
       <main className="flex max-w-screen max-h-screen flex-col items-center justify-between">
         <div className="flex flex-col items-center justify-center gap-10 h-[80vh]">
-          <Image src={background} alt="" className="hidden md:block max-w-[716px] max-h-auto"></Image>
-          <Image src={backgroundMobile} alt="" className="block md:hidden max-w-[614px] max-h-auto"></Image>
+          <Image src={"./assets/background.webp"} alt="" className="hidden md:block max-w-[716px] max-h-auto"></Image>
+          <Image src={"./assets/backgroundMobile.webp"} alt="" className="block md:hidden max-w-[614px] max-h-auto"></Image>
           <div className="flex flex-col items-center justify-center gap-3">
             <p>Bem-vindo ao PokeApp!</p>
             <ButtonC />
