9f2d6e631fd91e8c4b34aca612b9435305f6e515
maiconlara <maiconluan.lara@yahoo.com.br>
2023-10-05 14:01:15 -0300

    creating card layout

--- src/app/pokemons/page.tsx
+++ src/app/pokemons/page.tsx
@@ -3,12 +3,43 @@
 import SearchInput from "../components/SearchInput";
 const Pokemons = () => {
   return (
-        <main className="relative flex max-w-screen max-h-screen flex-col items-center justify-center">
-          <div className="absolute top-4 right-0">
+    <main className="relative flex max-w-screen flex-col items-center justify-center">
+      <div className="absolute top-10 right-2">
+        <SearchInput placeHolder="Pesquise um pokemon..." />
+      </div>
 
-          <SearchInput placeHolder="Pesquise um pokemon..." />
-          </div>
-        </main>
+      <div className="flex justify-center w-full min-h-[80vh] mt-20 pb-5">
+      <div className="flex flex-row justify-center w-full h-full">
+        <div className="flex flex-col m-4 p-4 gap-4 w-[20vw] rounded-lg">
+        <div className="w-[200px] h-[30px] border border-gray-300/40"></div>
+        <div className="w-[200px] h-[30px] border border-gray-300/40"></div>
+        <div className="w-[200px] h-[30px] border border-gray-300/40"></div>
+        <div className="w-[200px] h-[30px] border border-gray-300/40"></div>
+        </div>
+        <div className="flex flex-wrap m-4 w-[70vw] rounded-lg p-4 gap-4">
+       <div className="w-[150px] h-[150px] border border-gray-300/40"></div>
+       <div className="w-[150px] h-[150px] border border-gray-300/40"></div>
+       <div className="w-[150px] h-[150px] border border-gray-300/40"></div>
+       <div className="w-[150px] h-[150px] border border-gray-300/40"></div>
+       <div className="w-[150px] h-[150px] border border-gray-300/40"></div>
+       <div className="w-[150px] h-[150px] border border-gray-300/40"></div>
+       <div className="w-[150px] h-[150px] border border-gray-300/40"></div>
+       <div className="w-[150px] h-[150px] border border-gray-300/40"></div>
+       <div className="w-[150px] h-[150px] border border-gray-300/40"></div>
+       <div className="w-[150px] h-[150px] border border-gray-300/40"></div>
+       <div className="w-[150px] h-[150px] border border-gray-300/40"></div>
+       <div className="w-[150px] h-[150px] border border-gray-300/40"></div>
+       <div className="w-[150px] h-[150px] border border-gray-300/40"></div>
+       <div className="w-[150px] h-[150px] border border-gray-300/40"></div>
+       <div className="w-[150px] h-[150px] border border-gray-300/40"></div>
+       <div className="w-[150px] h-[150px] border border-gray-300/40"></div>
+       <div className="w-[150px] h-[150px] border border-gray-300/40"></div>
+       <div className="w-[150px] h-[150px] border border-gray-300/40"></div>
+       <div className="w-[150px] h-[150px] border border-gray-300/40"></div>
+        </div>
+      </div>
+      </div>
+    </main>
   );
 };
 export default Pokemons;
