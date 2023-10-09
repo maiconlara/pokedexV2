c1c4bec9cd15a345a825b4bce98b28902bffb6c3
maiconlara <maiconluan.lara@yahoo.com.br>
2023-10-09 11:10:12 -0300

    container component

---
+++ src/app/components/PokemonContainer/index.tsx
@@ -0,0 +1,38 @@
+const PokemonContainer = () => {
+  return (
+    <div className="flex justify-center w-full min-h-[80vh] mt-20 pb-5">
+      <div className="flex flex-col md:flex-row justify-center w-full h-full">
+        <div className="grid grid-cols-2 md:flex md:flex-col m-4 p-4 gap-4 md:-w-[20vw] rounded-lg">
+          <div className="w-full md:w-[200px] h-[30px] border border-gray-300/40"></div>
+          <div className="w-full md:w-[200px] h-[30px] border border-gray-300/40"></div>
+          <div className="w-full md:w-[200px] h-[30px] border border-gray-300/40"></div>
+          <div className="w-full md:w-[200px] h-[30px] border border-gray-300/40"></div>
+        </div>
+
+        <div className="grid grid-cols-2 md:flex md:flex-wrap m-4  rounded-lg p-4 gap-4">
+          <div className="w-full md:w-[150px] h-[150px] border border-gray-300/40"></div>
+          <div className="w-full md:w-[150px] h-[150px] border border-gray-300/40"></div>
+          <div className="w-full md:w-[150px] h-[150px] border border-gray-300/40"></div>
+          <div className="w-full md:w-[150px] h-[150px] border border-gray-300/40"></div>
+          <div className="w-full md:w-[150px] h-[150px] border border-gray-300/40"></div>
+          <div className="w-full md:w-[150px] h-[150px] border border-gray-300/40"></div>
+          <div className="w-full md:w-[150px] h-[150px] border border-gray-300/40"></div>
+          <div className="w-full md:w-[150px] h-[150px] border border-gray-300/40"></div>
+          <div className="w-full md:w-[150px] h-[150px] border border-gray-300/40"></div>
+          <div className="w-full md:w-[150px] h-[150px] border border-gray-300/40"></div>
+          <div className="w-full md:w-[150px] h-[150px] border border-gray-300/40"></div>
+          <div className="w-full md:w-[150px] h-[150px] border border-gray-300/40"></div>
+          <div className="w-full md:w-[150px] h-[150px] border border-gray-300/40"></div>
+          <div className="w-full md:w-[150px] h-[150px] border border-gray-300/40"></div>
+          <div className="w-full md:w-[150px] h-[150px] border border-gray-300/40"></div>
+          <div className="w-full md:w-[150px] h-[150px] border border-gray-300/40"></div>
+          <div className="w-full md:w-[150px] h-[150px] border border-gray-300/40"></div>
+          <div className="w-full md:w-[150px] h-[150px] border border-gray-300/40"></div>
+          <div className="w-full md:w-[150px] h-[150px] border border-gray-300/40"></div>
+          <div className="w-full md:w-[150px] h-[150px] border border-gray-300/40"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default PokemonContainer;
--- src/app/pokemons/page.tsx
+++ src/app/pokemons/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import PokemonContainer from "../components/PokemonContainer";
 import SearchInput from "../components/SearchInput";
 import { useState } from "react";
 const Pokemons = () => {
@@ -9,42 +10,8 @@ const Pokemons = () => {
       <div className="absolute top-10 md:right-2 w-full md:w-auto px-6 md:px-4 ">
         <SearchInput setSearchText={setSearchText} placeHolder="Pesquise um pokemon..." />
       </div>
+        <PokemonContainer />
 
-      <div className="flex justify-center w-full min-h-[80vh] mt-20 pb-5">
-      <div className="flex flex-col md:flex-row justify-center w-full h-full">
-
-        <div className="grid grid-cols-2 md:flex md:flex-col m-4 p-4 gap-4 md:-w-[20vw] rounded-lg">
-        <div className="w-full md:w-[200px] h-[30px] border border-gray-300/40"></div>
-        <div className="w-full md:w-[200px] h-[30px] border border-gray-300/40"></div>
-        <div className="w-full md:w-[200px] h-[30px] border border-gray-300/40"></div>
-        <div className="w-full md:w-[200px] h-[30px] border border-gray-300/40"></div>
-        </div>
-
-        <div className="grid grid-cols-2 md:flex md:flex-wrap m-4  rounded-lg p-4 gap-4">
-       <div className="w-full md:w-[150px] h-[150px] border border-gray-300/40"></div>
-       <div className="w-full md:w-[150px] h-[150px] border border-gray-300/40"></div>
-       <div className="w-full md:w-[150px] h-[150px] border border-gray-300/40"></div>
-       <div className="w-full md:w-[150px] h-[150px] border border-gray-300/40"></div>
-       <div className="w-full md:w-[150px] h-[150px] border border-gray-300/40"></div>
-       <div className="w-full md:w-[150px] h-[150px] border border-gray-300/40"></div>
-       <div className="w-full md:w-[150px] h-[150px] border border-gray-300/40"></div>
-       <div className="w-full md:w-[150px] h-[150px] border border-gray-300/40"></div>
-       <div className="w-full md:w-[150px] h-[150px] border border-gray-300/40"></div>
-       <div className="w-full md:w-[150px] h-[150px] border border-gray-300/40"></div>
-       <div className="w-full md:w-[150px] h-[150px] border border-gray-300/40"></div>
-       <div className="w-full md:w-[150px] h-[150px] border border-gray-300/40"></div>
-       <div className="w-full md:w-[150px] h-[150px] border border-gray-300/40"></div>
-       <div className="w-full md:w-[150px] h-[150px] border border-gray-300/40"></div>
-       <div className="w-full md:w-[150px] h-[150px] border border-gray-300/40"></div>
-       <div className="w-full md:w-[150px] h-[150px] border border-gray-300/40"></div>
-       <div className="w-full md:w-[150px] h-[150px] border border-gray-300/40"></div>
-       <div className="w-full md:w-[150px] h-[150px] border border-gray-300/40"></div>
-       <div className="w-full md:w-[150px] h-[150px] border border-gray-300/40"></div>
-       <div className="w-full md:w-[150px] h-[150px] border border-gray-300/40"></div>
-     
-        </div>
-      </div>
-      </div>
     </main>
   );
 };
