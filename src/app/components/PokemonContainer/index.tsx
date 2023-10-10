1613525b3d24897280581ed99ea9ae6dd1e937f6
maiconlara <maiconluan.lara@yahoo.com.br>
2023-10-10 16:32:53 -0300

    items-start in container

--- src/app/components/PokemonContainer/index.tsx
+++ src/app/components/PokemonContainer/index.tsx
@@ -3,9 +3,9 @@ import TypeFilter from "../TypeFilter";
 const PokemonContainer = () => {
   return (
     <div className="flex justify-center w-full min-h-[80vh] mt-20 pb-5">
-      <div className="flex flex-col md:flex-row justify-center w-full h-full">
+      <div className="flex flex-col md:flex-row justify-center items-start w-full h-full">
         <TypeFilter />
-        <div className="grid grid-cols-2 md:flex md:flex-wrap m-4 rounded-lg p-4 gap-4">
+        <div className="grid grid-cols-2 md:flex md:flex-row md:flex-wrap m-4  rounded-lg p-4 gap-4">
           <div className="w-full md:w-[150px] h-[150px] border border-gray-300/40"></div>
           <div className="w-full md:w-[150px] h-[150px] border border-gray-300/40"></div>
           <div className="w-full md:w-[150px] h-[150px] border border-gray-300/40"></div>
@@ -14,18 +14,7 @@ const PokemonContainer = () => {
           <div className="w-full md:w-[150px] h-[150px] border border-gray-300/40"></div>
           <div className="w-full md:w-[150px] h-[150px] border border-gray-300/40"></div>
           <div className="w-full md:w-[150px] h-[150px] border border-gray-300/40"></div>
-          <div className="w-full md:w-[150px] h-[150px] border border-gray-300/40"></div>
-          <div className="w-full md:w-[150px] h-[150px] border border-gray-300/40"></div>
-          <div className="w-full md:w-[150px] h-[150px] border border-gray-300/40"></div>
-          <div className="w-full md:w-[150px] h-[150px] border border-gray-300/40"></div>
-          <div className="w-full md:w-[150px] h-[150px] border border-gray-300/40"></div>
-          <div className="w-full md:w-[150px] h-[150px] border border-gray-300/40"></div>
-          <div className="w-full md:w-[150px] h-[150px] border border-gray-300/40"></div>
-          <div className="w-full md:w-[150px] h-[150px] border border-gray-300/40"></div>
-          <div className="w-full md:w-[150px] h-[150px] border border-gray-300/40"></div>
-          <div className="w-full md:w-[150px] h-[150px] border border-gray-300/40"></div>
-          <div className="w-full md:w-[150px] h-[150px] border border-gray-300/40"></div>
-          <div className="w-full md:w-[150px] h-[150px] border border-gray-300/40"></div>
+
         </div>
       </div>
     </div>
