f9041ab9f81015738164d9c92c67fa06418f127e
maiconlara <maiconluan.lara@yahoo.com.br>
2023-10-10 16:36:51 -0300

    fixing container layout in mobile

--- src/app/components/PokemonContainer/index.tsx
+++ src/app/components/PokemonContainer/index.tsx
@@ -3,7 +3,7 @@ import TypeFilter from "../TypeFilter";
 const PokemonContainer = () => {
   return (
     <div className="flex justify-center w-full min-h-[80vh] mt-20 pb-5">
-      <div className="flex flex-col md:flex-row justify-center items-start w-full h-full">
+      <div className="flex flex-col md:flex-row justify-center md:items-start w-full h-full">
         <TypeFilter />
         <div className="grid grid-cols-2 md:flex md:flex-row md:flex-wrap m-4  rounded-lg p-4 gap-4">
           <div className="w-full md:w-[150px] h-[150px] border border-gray-300/40"></div>
