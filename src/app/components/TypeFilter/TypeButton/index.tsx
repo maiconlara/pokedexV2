ef431aa57a71b67e5efff1c82f340ecb0d3a5df4
Maicon <maiconluan.lara@yahoo.com.br>
2023-12-07 09:48:59 -0300

    refactor: adding hover in type filter

--- src/app/components/TypeFilter/TypeButton/index.tsx
+++ src/app/components/TypeFilter/TypeButton/index.tsx
@@ -7,7 +7,7 @@ interface TypeButtonProps {
 
 const TypeButton = ({ type, title }: TypeButtonProps) => {
   return (
-    <div className="w-full flex flex-row gap-3 md:w-[200px] h-[30px] items-center">
+    <div className="w-full flex flex-row gap-3 md:w-[150px] items-center rounded-lg px-2 py-1 cursor-pointer hover:bg-white/10">
       <Image src={type} alt="" width={24} height={24}></Image>
       <p className="font-medium text-base">{title}</p>
     </div>
