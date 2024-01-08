a67a641dabecbdc4b806ceef44fb2136c781b4b9
maiconlara <maiconluan.lara@yahoo.com.br>
2024-01-08 16:04:54 -0300

    feat: pokemon card

---
+++ src/app/components/pokemon-card.tsx
@@ -0,0 +1,10 @@
+const PokemonCard = () => {
+  return (
+    <div className="w-full md:w-[250px] h-[150px] border border-gray-300/40 rounded-2xl bg-sky-300/40">
+
+
+
+    </div>
+  );
+};
+export default PokemonCard;
