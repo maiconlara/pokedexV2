0a7d5b78888da4f78df2aba208f74ca017144a4a
maiconlara <maiconluan.lara@yahoo.com.br>
2024-01-08 16:12:23 -0300

    refactor: fixing distance between search and pokemon container

--- src/app/components/PokemonContainer/index.tsx
+++ src/app/components/PokemonContainer/index.tsx
@@ -3,7 +3,7 @@ import PokemonCard from "../pokemon-card";
 
 const PokemonContainer = () => {
   return (
-    <div className="flex justify-center w-full min-h-[80vh] mt-20 pb-5">
+    <div className="flex justify-center w-full min-h-[80vh] mt-[5.60rem] pb-5">
       <div className="flex flex-col md:flex-row justify-center md:items-start w-full h-full">
         <TypeFilter />
         <div className="grid grid-cols-1 md:flex md:flex-row md:flex-wrap md:min-w-[85vw] md:max-w-full m-4 rounded-lg p-4 gap-4">
