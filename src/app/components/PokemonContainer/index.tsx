77c0b52e8b148d634deacc1f6bd44b6ba5dd2b0a
maiconlara <maiconluan.lara@yahoo.com.br>
2024-01-08 16:05:03 -0300

    refactor: adding pokemon card to container

--- src/app/components/PokemonContainer/index.tsx
+++ src/app/components/PokemonContainer/index.tsx
@@ -1,20 +1,21 @@
 import TypeFilter from "../TypeFilter";
+import PokemonCard from "../pokemon-card";
 
 const PokemonContainer = () => {
   return (
     <div className="flex justify-center w-full min-h-[80vh] mt-20 pb-5">
       <div className="flex flex-col md:flex-row justify-center md:items-start w-full h-full">
         <TypeFilter />
-        <div className="grid grid-cols-2 md:flex md:flex-row md:flex-wrap m-4  rounded-lg p-4 gap-4">
-          <div className="w-full md:w-[150px] h-[150px] border border-gray-300/40"></div>
-          <div className="w-full md:w-[150px] h-[150px] border border-gray-300/40"></div>
-          <div className="w-full md:w-[150px] h-[150px] border border-gray-300/40"></div>
-          <div className="w-full md:w-[150px] h-[150px] border border-gray-300/40"></div>
-          <div className="w-full md:w-[150px] h-[150px] border border-gray-300/40"></div>
-          <div className="w-full md:w-[150px] h-[150px] border border-gray-300/40"></div>
-          <div className="w-full md:w-[150px] h-[150px] border border-gray-300/40"></div>
-          <div className="w-full md:w-[150px] h-[150px] border border-gray-300/40"></div>
-
+        <div className="grid grid-cols-1 md:flex md:flex-row md:flex-wrap md:min-w-[85vw] md:max-w-full m-4 rounded-lg p-4 gap-4">
+          <PokemonCard />
+          <PokemonCard />
+          <PokemonCard />
+          <PokemonCard />
+          <PokemonCard />
+          <PokemonCard />
+          <PokemonCard />
+          <PokemonCard />
+          <PokemonCard />
         </div>
       </div>
     </div>
