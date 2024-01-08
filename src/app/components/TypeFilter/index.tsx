e70aa020a614c56bf3a41ac0a87c0db886f2fdce
maiconlara <maiconluan.lara@yahoo.com.br>
2024-01-08 15:58:47 -0300

    refactor: hidding type filter in mobile

--- src/app/components/TypeFilter/index.tsx
+++ src/app/components/TypeFilter/index.tsx
@@ -4,7 +4,7 @@ import TypeButton from "./TypeButton";
 
 const TypeFilter = () => {
   return (
-    <div className="grid grid-cols-2 md:flex md:flex-col m-4 p-4 gap-2 md:-w-[20vw] rounded-lg">
+    <div className="hidden md:flex md:flex-col m-4 p-4 gap-2 md:-w-[20vw] rounded-lg">
       {types.map((type) => {
         return <TypeButton type={type.type} title={type.name} key={type.id} />;
       })}
