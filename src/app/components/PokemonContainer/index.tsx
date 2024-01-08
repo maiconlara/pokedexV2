dc1ac87c66973795a1ba506e8f33f46bca857899
maiconlara <maiconluan.lara@yahoo.com.br>
2024-01-08 16:18:06 -0300

    refactor: layout changes

--- src/app/components/PokemonContainer/index.tsx
+++ src/app/components/PokemonContainer/index.tsx
@@ -6,7 +6,7 @@ const PokemonContainer = () => {
     <div className="flex justify-center w-full min-h-[80vh] mt-[5.60rem] pb-5">
       <div className="flex flex-col md:flex-row justify-center md:items-start w-full h-full">
         <TypeFilter />
-        <div className="grid grid-cols-1 md:flex md:flex-row md:flex-wrap md:min-w-[85vw] md:max-w-full m-4 rounded-lg p-4 gap-4">
+        <div className="grid grid-cols-1 md:flex md:flex-row md:flex-wrap md:min-w-[55vw] md:max-w-full  lg:min-w-[80vw] lg:max-w-full m-4 rounded-lg p-4 gap-4">
           <PokemonCard />
           <PokemonCard />
           <PokemonCard />
--- src/app/components/pokemon-card.tsx
+++ src/app/components/pokemon-card.tsx
@@ -1,6 +1,6 @@
 const PokemonCard = () => {
   return (
-    <div className="w-full md:w-[250px] h-[150px] border border-gray-300/40 rounded-2xl bg-sky-300/40">
+    <div className="w-full md:w-[200px] h-[150px] lg:w-[250px]  border border-gray-300/40 rounded-2xl bg-sky-300/40">
 
 
 
