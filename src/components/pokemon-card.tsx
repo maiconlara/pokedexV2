43b295e501f4cd847274a73861b03efd929b13f5
maiconlara <maiconluan.lara@yahoo.com.br>
2024-01-10 14:55:26 -0300

    refactor: upgrading responsivity

--- src/components/pokemon-card.tsx
+++ src/components/pokemon-card.tsx
@@ -5,7 +5,7 @@ interface PokemonCardProps {
 
 const PokemonCard = ({name}: PokemonCardProps) => {
   return (
-    <div className="w-full h-[150px] md:w-[180px] lg:w-[200px] xl:w-[210px] 2xl:w-[220px] border border-gray-300/40 rounded-2xl bg-sky-300/40">
+    <div className="w-full h-[150px] md:w-[180px] lg:w-[200px] xl:w-[290px] 2xl:w-[220px] border border-gray-300/40 rounded-2xl bg-sky-300/40">
       {name}
     </div>
   );
--- src/components/pokemon-list.tsx
+++ src/components/pokemon-list.tsx
@@ -5,7 +5,7 @@ const PokemonList = () => {
   const { data: { results: pokemons = [] } = {} } = useGetPokemons();
 
   return (
-    <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-5 md:w-[376px] lg:w-[632px] xl:w-[662px] 2xl:w-[1164px]  ">
+    <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-5 md:w-[376px] lg:w-[632px] xl:w-[902px] 2xl:w-[1164px]  ">
       {pokemons.map((pokemon) => {
         return <PokemonCard key={pokemon.name} name={pokemon.name} />;
       })}
