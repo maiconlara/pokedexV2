f0b15fce341d32490ae2e6931676530d14aa194e
maiconlara <maiconluan.lara@yahoo.com.br>
2024-01-11 14:06:04 -0300

    refactor: starting pokemon cards

--- src/components/pokemon-card.tsx
+++ src/components/pokemon-card.tsx
@@ -1,13 +1,56 @@
-
+import { useGetPokemonData } from "@/utils/hooks/useGetPokemonData";
+// import Image from "next/image";
+import { Skeleton, Image } from "@nextui-org/react";
+import fallBackImage from "@/app/icon.png";
+import { handleTypeColors } from "@/utils/handleTypeColor";
 interface PokemonCardProps {
-  name: string
+  id: string;
 }
 
-const PokemonCard = ({name}: PokemonCardProps) => {
+const PokemonCard = ({ id }: PokemonCardProps) => {
+  const { data, isLoading } = useGetPokemonData(id);
+  const firstType = data?.types[0]?.type.name || "";
+  const secondType = data?.types[1]?.type.name || "";
+  const name = data?.name || "";
+  const formattedName = name.charAt(0).toUpperCase() + name.slice(1);
+  const typeColor = handleTypeColors(firstType);
   return (
-    <div className="w-full h-[150px] md:w-[180px] lg:w-[200px] xl:w-[290px] 2xl:w-[220px] border border-gray-300/40 rounded-2xl bg-sky-300/40">
-      {name}
-    </div>
+    <Skeleton
+      isLoaded={!isLoading}
+      disableAnimation
+      className="rounded-lg"
+      classNames={{
+        base: ["bg-[#aaa]"],
+      }}
+    >
+      <div
+        style={typeColor}
+        className="flex flex-col w-full items-center justify-center h-[150px] md:w-[180px] lg:w-[200px] xl:w-[290px] 2xl:w-[220px] rounded-2xl"
+      >
+        <div className="flex flex-row w-full justify-between px-5">
+          <p>{formattedName}</p>
+          <p>{data?.id}</p>
+        </div>
+
+        <div className="flex flex-row w-full h-full items-center">
+          <div className="flex flex-col w-auto px-5">
+            {firstType && <p>{firstType}</p>}
+            {secondType && <p>{secondType}</p>}
+          </div>
+
+          <Image
+            className="cursor-pointer rounded-2xl"
+            loading="lazy"
+            isZoomed
+            src={data?.sprites.other["official-artwork"].front_default || ""}
+            alt={data?.name}
+            width={100}
+            disableSkeleton
+            height={100}
+          />
+        </div>
+      </div>
+    </Skeleton>
   );
 };
 export default PokemonCard;
--- src/components/pokemon-list.tsx
+++ src/components/pokemon-list.tsx
@@ -1,13 +1,28 @@
 import PokemonCard from "./pokemon-card";
 import { useGetPokemons } from "@/utils/hooks/useGetPokemons";
 
+
+
+interface FormattedPokemons {
+  id: string;
+}
+
 const PokemonList = () => {
   const { data: { results: pokemons = [] } = {} } = useGetPokemons();
+  
+  const formattedPokemons: FormattedPokemons[] = pokemons.map(pokemon => {
+    const urlParts = pokemon.url.split('/');
+    const id = urlParts[urlParts.length - 2]; // O ID está na penúltima parte da URL
+  
+    return {
+      id,
+    };
+  });
 
   return (
     <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-5 md:w-[376px] lg:w-[632px] xl:w-[902px] 2xl:w-[1164px]  ">
-      {pokemons.map((pokemon) => {
-        return <PokemonCard key={pokemon.name} name={pokemon.name} />;
+      {formattedPokemons.map((pokemon) => {
+        return <PokemonCard key={pokemon.id} id={pokemon.id} />;
       })}
     </div>
   );
