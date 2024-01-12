d3ba06191124c114ae5e99b3728ff384219e2a6c
maiconlara <maiconluan.lara@yahoo.com.br>
2024-01-12 10:17:35 -0300

    feat: hooks to handle colors by type

--- src/utils/handleTypeColor.ts
+++ src/utils/handleTypeColor.ts
@@ -1,64 +1,84 @@
-export const handleTypeColors = (type: string) => {
-  const pokemonTypes: Record<string, Record<string, string>> = {
-    normal: {
-      backgroundColor: "#a8a878",
-    },
-    fire: {
-      backgroundColor: "#f08030",
-    },
-    fighting: {
-      backgroundColor: "#c03028",
-    },
-    water: {
-      backgroundColor: "#6890f0",
-    },
-    flying: {
-      backgroundColor: "#a890f0",
-    },
-    grass: {
-      backgroundColor: "#78c850",
-    },
-    poison: {
-      backgroundColor: "#a040a0",
-    },
-    eletric: {
-        backgroundColor: "#f8d030",
-    },
-    ground: {
-        backgroundColor: "#e0c068",
-    },
-    pyschic: {
-        backgroundColor: "#f85888",
-    },
-    rock: {
-        backgroundColor: "#b8a038",
-    },
-    ice: {
-        backgroundColor: "#98d8d8",
-},
-    bug: {
-        backgroundColor: "#a8b820",
-    },
-    dragon: {
-        backgroundColor: "#7038f8",
-    },
-    ghost: {
-        backgroundColor: "#705898",
-    },
-    dark: {
-        backgroundColor: "#705848",
-    },
-    steel: {
-        backgroundColor: "#b8b8d0",
-    },
-    fairy: {
-        backgroundColor: "#ee99ac",
-    },
+export const handleTypeColor = (type: string) => {
+    const pokemonTypes: Record<string, Record<string, string>> = {
+      normal: {
+        backgroundColor: "#858566", 
+        border: "1px solid #6d6d4e",
+      },
+      fire: {
+        backgroundColor: "#c26e32",
+        border: "1px solid #9c531f",
+      },
+      fighting: {
+        backgroundColor: "#c03028",
+        border: "1px solid #2c2c31",
+      },
+      water: {
+        backgroundColor: "#6281cc",
+        border: "1px solid #445e9c",
+      },
+      flying: {
+        backgroundColor: "#a890f0",
+        border: "1px solid #2c2c31",
+      },
+      grass: {
+        backgroundColor: "#58913c",
+        border: "1px solid #4e8234",
+      },
+      poison: {
+        backgroundColor: "#7d3c7d",
+        border: "1px solid #682a68",
+      },
+      electric: {
+          backgroundColor: "#d6ae0f",
+           border: "1px solid #a1871f",
+      },
+      ground: {
+          backgroundColor: "#ad985e",
+           border: "1px solid #927d44",
+      },
+      pyschic: {
+          backgroundColor: "#f85888",
+           border: "1px solid #2c2c31",
+      },
+      rock: {
+          backgroundColor: "#b8a038",
+           border: "1px solid #2c2c31",
+      },
+      ice: {
+          backgroundColor: "#98d8d8",
+           border: "1px solid #2c2c31",
+  },
+      bug: {
+          backgroundColor: "#8a9623",
+           border: "1px solid #6d7815",
+      },
+      dragon: {
+          backgroundColor: "#7038f8",
+           border: "1px solid #2c2c31",
+      },
+      ghost: {
+          backgroundColor: "#705898",
+           border: "1px solid #2c2c31",
+      },
+      dark: {
+          backgroundColor: "#705848",
+           border: "1px solid #2c2c31",
+      },
+      steel: {
+          backgroundColor: "#b8b8d0",
+           border: "1px solid #2c2c31",
+      },
+      fairy: {
+          backgroundColor: "#ee99ac",
+           border: "1px solid #2c2c31",
+      },
+    };
+  
+    return (
+      pokemonTypes[type] || {
+        backgroundColor: "#a8a878",
+         border: "1px solid #2c2c31",
+      }
+    );
   };
-
-  return (
-    pokemonTypes[type] || {
-      backgroundColor: "#a8a878",
-    }
-  );
-};
+  
