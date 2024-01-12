1341600b4872ef8521ab2f67e1a168f6d1117322
maiconlara <maiconluan.lara@yahoo.com.br>
2024-01-12 13:14:12 -0300

    refactor: translation minor changes

--- src/components/pokemon-type.tsx
+++ src/components/pokemon-type.tsx
@@ -1,15 +1,19 @@
+import { useTranslation } from 'next-i18next'
+
 interface PokemonTypeProps {
   type: string;
 }
 
 const PokemonType = ({ type }: PokemonTypeProps) => {
 
- const formattedType = type.charAt(0).toUpperCase() + type.slice(1);
+  const {t} = useTranslation()
+
+//  const formattedType = type.charAt(0).toUpperCase() + type.slice(1);
   return <div className="flex min-w-[50px] w-full bg-[#ad985e] 
   rounded-full items-center justify-center px-2 py-1 border-1 border-[#927d44]">
     <p>
-
-    {formattedType}
+    {t(type)}
+    {/* {formattedType} */}
     </p>
     </div>;
 };
--- src/components/type-button.tsx
+++ src/components/type-button.tsx
@@ -1,15 +1,26 @@
 import Image from "next/image";
-
+import { useQueryState } from "nuqs";
+import { useTranslation } from "next-i18next";
 interface TypeButtonProps {
   type: string;
   title: string;
 }
 
 const TypeButton = ({ type, title }: TypeButtonProps) => {
+  const { t } = useTranslation();
+
+  const setTypeParam = useQueryState("type")[1];
+  const handleTypeParam = () => {
+    setTypeParam(title);
+  };
+
   return (
-    <div className="w-full flex flex-row gap-3 md:w-[150px] items-center rounded-lg px-2 py-1 transition-colors cursor-pointer hover:bg-white/10">
+    <div
+      onClick={handleTypeParam}
+      className="w-full flex flex-row gap-3 items-center rounded-lg px-2 py-1 transition-colors cursor-pointer hover:bg-white/10"
+    >
       <Image src={type} alt="" width={24} height={24}></Image>
-      <p className="font-medium text-base">{title}</p>
+      <p className="font-medium text-base">{t(title)}</p>
     </div>
   );
 };
