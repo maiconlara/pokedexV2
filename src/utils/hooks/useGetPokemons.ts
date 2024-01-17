eaf772a87e6784b603a115a34e00796ea1254b28
maiconlara <maiconluan.lara@yahoo.com.br>
2024-01-17 10:32:39 -0300

    refactor: minor change

--- src/utils/hooks/useGetPokemons.ts
+++ src/utils/hooks/useGetPokemons.ts
@@ -3,14 +3,17 @@ import { api } from "@/lib/api";
 import { PokemonListResponse } from "@/types/pokemon-list";
 
 const getPokemonsRequest = async () => {
-    const { data } = await api.get<PokemonListResponse>("/pokemon/", {
-        params: {
-            limit: 30,
-        },
-    });
-    return data;
+  const { data } = await api.get<PokemonListResponse>("/pokemon/", {
+    params: {
+      limit: 30,
+    },
+  });
+  return data;
 };
 
 export const useGetPokemons = () => {
-    return useQuery({queryKey: ["pokemonResponse"], queryFn: getPokemonsRequest});
+  return useQuery({
+    queryKey: ["pokemonResponse"],
+    queryFn: getPokemonsRequest,
+  });
 };
